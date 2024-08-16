cd4317b2c9deb7a908f8fff1c5709c8fd47c1ce0
Olivia Hu <oliviahu2020@gmail.com>
2024-08-16 12:34:31 -0400

    Adjusts interval

--- src/Timer.js
+++ src/Timer.js
@@ -76,7 +76,7 @@ function Timer( ) {
 
     const minutes = Math.floor(secondsLeftRef.current / 60);
     let seconds = secondsLeftRef.current % 60;
-    if (seconds < 10) seconds = '0' + seconds;
+    if (seconds < 1000) seconds = '0' + seconds;
 
     return(
         <div>
